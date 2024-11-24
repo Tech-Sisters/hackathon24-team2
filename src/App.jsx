fa71f0157d1ea2c06d24aa0bbbaa6564be1c0e80
smeea-2018 <smeeaarshad@gmail.com>
2024-11-24 21:57:56 +0000

    qa qa!qa!

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Landing from "./pages/landing";
-import Emotes from "./pages/emotes"
+import Emotes from "./pages/emotes";
 import Chatbot from "./pages/chatbot";
-
+import * as React from "react";
+import FeelingPage from "./components/feelingPage";
+import EmotionPage from "./components/emotionPage";
+import ActivitiesPage from "./components/activitiesPage";
 function App() {
-
   return (
     <>
       <Router>
         <div>
+          {/* <nav>
+            <Link to="/feeling">Feeling</Link>
+            <Link to="/emotion">Emotion</Link>
+            <Link to="/activities">Activities</Link>
+          </nav> */}
+
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/emote" element={<Emotes />} />
-            <Route path="/maia" element={<Chatbot /> } />
+            <Route path="/maia" element={<Chatbot />} />
+            <Route path="/feeling" element={<FeelingPage />} />
+            <Route path="/emotion" element={<EmotionPage />} />
+            <Route path="/activities" element={<ActivitiesPage />} />
           </Routes>
         </div>
       </Router>
