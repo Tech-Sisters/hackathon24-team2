277d02b6f3bd658b706e35f947c569a1b88fd4f5
Maryam Islam <116493523+marzcreatives@users.noreply.github.com>
2024-11-27 00:08:29 +0000

    centralised tracked data

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,19 @@
-import { Box, AppBar } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import Icon from '../assets/icon.jsx';
+import { Box, AppBar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Icon from "../assets/icon.jsx";
 import "../App.css";
 
 export const Header = () => {
   const navigate = useNavigate();
 
   const handleNavigation = () => {
-    navigate("/"); 
+    navigate("/");
   };
-
 
   return (
     <AppBar id="header" position="fixed" onClick={handleNavigation}>
       <Box id="headerBar">
-      <Icon className="headerText" alt="MAIA" icon="logo" id="iconSmall" />
+        <Icon className="headerText" alt="MAIA" icon="logo" id="iconSmall" />
       </Box>
     </AppBar>
   );
