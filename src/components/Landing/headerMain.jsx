ae2f0ce55fbaaa9135e34674da5c210aadb6d512
Maryam Islam <116493523+marzcreatives@users.noreply.github.com>
2024-11-27 22:46:55 +0000

    logo header

--- src/components/Landing/headerMain.jsx
+++ src/components/Landing/headerMain.jsx
@@ -1,19 +1,14 @@
-import AppBar from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { createTheme } from '@mui/material/styles';
+import {AppBar, Box} from '@mui/material';
+import Icon from '../../assets/icon.jsx';
 import "./landing.css"
 
-const theme = createTheme();
-
 export default function HeaderMain() {
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
       <AppBar id="headerMain">
-          <Typography id="headerLogo" variant={isMobile ? 'h3' : 'h2'} >
-            MAIA
-          </Typography>
+        <Box id="headerBox">
+            <Icon alt="MAIA" icon="logo" id="iconLarge" /> 
+        </Box>
       </AppBar>
   );
 }
