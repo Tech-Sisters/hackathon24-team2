4f7c60cebeb69d87ff931f02fc6dc7b6abcc2d56
Maryam Islam <116493523+marzcreatives@users.noreply.github.com>
2024-11-28 22:04:59 +0000

    styling

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,3 +24,11 @@ export const emotionAccentColors = {
     veryGood: "var(--veryGoodAccent)",
   };
   
+export const emotionTransparentColors = {
+    veryBad: "rgba(200, 235, 235, 0.5)", 
+    bad: "rgba(255, 216, 206, 0.5)", 
+    neutral: "rgba(200, 235, 200, 0.5)",
+    good: "rgba(251, 223, 255, 0.5)",
+    veryGood: "rgba(255, 238, 184, 0.5)",
+    white: "rgba(255, 255, 255, 0.5)",
+  };
